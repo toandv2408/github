d10508712953302cc65824e23f41e39062662d11
Toan Dv <toando2408@gmail.com>
2023-03-14 01:47:24 +0700

    add bootstrap.php file

---
+++ bootstrap.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+    <title>Bootstrap</title>
+</head>
+
+<body>
+    <ion-icon name="heart"></ion-icon>
+    <ion-icon name="airplane-outline"></ion-icon>
+</body>
+
+</html>
